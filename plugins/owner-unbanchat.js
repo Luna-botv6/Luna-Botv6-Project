b61c7d8e1e2ded10f2f0fadef65a3ad2138eb2f8
Luna-botv6 <lunabotv6@gmail.com>
2025-05-26 04:59:45 -0300

    owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,15 +1,27 @@
 
-const handler = async (m) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.owner_unbanchat
+import fs from 'fs';
+import { getChat, setChat } from '../lib/chatsDB.js';
 
-  global.db.data.chats[m.chat].isBanned = false;
+const handler = async (m) => {
+ 
+  const idioma = 'es';
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`));
+  const tradutor = _translate.plugins.owner_unbanchat;
+
+  // Obtener datos actuales del chat
+  const chatData = getChat(m.chat);
+  chatData.isBanned = false;
+
+  // Guardar los cambios en chats.json
+  setChat(m.chat, chatData);
+
+  // Responder al usuario
   m.reply(tradutor.texto1);
 };
+
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
 handler.command = /^unbanchat$/i;
 handler.rowner = true;
+
 export default handler;
