badca53e683410058b9daee0950f833e788fd56e
Luna-botv6 <lunabotv6@gmail.com>
2025-05-26 04:55:02 -0300

    owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,15 +1,31 @@
+import fs from 'fs';
+import { getUser } from '../lib/usersDB.js';
+import { getChat, setChat } from '../lib/chatsDB.js';
 
 const handler = async (m) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.owner_banchat
+  // Obtenemos la info del usuario desde la base modular
+  const user = getUser(m.sender);
 
-  global.db.data.chats[m.chat].isBanned = true;
+  // Si el usuario no tiene idioma, usamos el idioma por defecto global
+  const idioma = user.language || global.defaultLenguaje;
+
+  // Cargamos las traducciones para ese idioma
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`));
+  const tradutor = _translate.plugins.owner_banchat;
+
+  // Obtenemos la info del chat
+  const chat = getChat(m.chat);
+
+  // Marcamos el chat como baneado (isBanned = true)
+  setChat(m.chat, { ...chat, isBanned: true });
+
+  // Respondemos
   m.reply(tradutor.texto1);
 };
+
 handler.help = ['banchat'];
 handler.tags = ['owner'];
 handler.command = /^banchat$/i;
 handler.rowner = true;
+
 export default handler;
