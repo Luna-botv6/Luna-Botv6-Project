ec5a344b785e45f44e7ad99b23e5db29554548c7
Luna-botv6 <lunabotv6@gmail.com>
2025-06-10 17:35:15 -0300

    Add files via upload

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,22 +1,16 @@
 import fs from 'fs';
-import { getUser, setUser } from '../lib/usersDB.js';
 
-const handler = async (m, { conn, usedPrefix, command }) => {
-  // Obtener datos del usuario que envía el mensaje para idioma
-  const userSender = getUser(m.sender);
-  const idioma = userSender.language || global.defaultLenguaje;
-
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`));
+const handler = async (m, { conn, participants, usedPrefix, command }) => {
+  const datas = global;
+  const idioma = datas.db.data.users[m.sender]?.language || global.defaultLenguaje;
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`, 'utf8'));
   const tradutor = _translate.plugins.owner_banuser;
 
   const BANtext = `${tradutor.texto1}\n*${usedPrefix + command} @${global.suittag}*`;
-
-  // Verificar que haya usuario mencionado o citado
   if (!m.mentionedJid?.[0] && !m.quoted) {
     return m.reply(BANtext, m.chat, { mentions: conn.parseMention(BANtext) });
   }
 
-  // Identificar al usuario a banear
   let who;
   if (m.isGroup) {
     who = m.mentionedJid?.[0] ? m.mentionedJid[0] : m.quoted.sender;
@@ -24,15 +18,18 @@ const handler = async (m, { conn, usedPrefix, command }) => {
     who = m.chat;
   }
 
-  // Obtener datos del usuario a banear
-  const userToBan = getUser(who);
+  const users = global.db.data.users;
 
-  // Actualizar propiedad banned a true
-  setUser(who, { ...userToBan, banned: true });
+  // Asegurarse de que el usuario exista en la base de datos
+  if (!users[who]) {
+    users[who] = {};
+  }
 
+  users[who].banned = true;
   m.reply(tradutor.texto2);
 };
 
 handler.command = /^banuser$/i;
 handler.rowner = true;
 export default handler;
+
