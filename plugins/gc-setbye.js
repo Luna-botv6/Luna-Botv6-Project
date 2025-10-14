d678b6204e5a51beaf7404449828dd1f4d9c38f8
Luna-botv6 <lunabotv6@gmail.com>
2025-10-14 16:34:55 -0300

    Add files via upload

--- plugins/gc-setbye.js
+++ plugins/gc-setbye.js
@@ -4,7 +4,6 @@ import { setConfig } from '../lib/funcConfig.js';
 const cooldowns = new Map();
 
 const handler = async (m, { isOwner, conn, text, args, usedPrefix }) => {
-  const chatId = m.chat;
   const cooldownTime = 2 * 60 * 1000;
   const now = Date.now();
 
@@ -22,8 +21,8 @@ const handler = async (m, { isOwner, conn, text, args, usedPrefix }) => {
   const isUserAdmin = groupAdmins.includes(realUserJid);
   if (!isUserAdmin && !isOwner) return m.reply('⚠️ Este comando solo puede ser usado por administradores del grupo.');
 
-  if (cooldowns.has(chatId)) {
-    const expirationTime = cooldowns.get(chatId) + cooldownTime;
+  if (cooldowns.has(m.chat)) {
+    const expirationTime = cooldowns.get(m.chat) + cooldownTime;
     if (now < expirationTime) {
       const timeLeft = Math.ceil((expirationTime - now) / 1000);
       const minutes = Math.floor(timeLeft / 60);
@@ -40,11 +39,11 @@ const handler = async (m, { isOwner, conn, text, args, usedPrefix }) => {
     return m.reply(`${tradutor.texto2}\n*- @user ${tradutor.texto3}`);
   }
 
-  global.db.data.chats[chatId].sBye = text;
-  await setConfig(chatId, { sBye: text });
+  global.db.data.chats[m.chat].sBye = text;
+  await setConfig(m.chat, { sBye: text });
   m.reply(tradutor.texto1);
 
-  cooldowns.set(chatId, now);
+  cooldowns.set(m.chat, now);
 };
 
 handler.help = ['setbye <text>'];
