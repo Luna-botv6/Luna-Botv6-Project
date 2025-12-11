f42547cd7d5e47ce0382ac06d35d588e62d37b2a
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2025-12-11 20:51:49 -0300

    Update futanri-mp4.js

--- plugins/futanri-mp4.js
+++ plugins/futanri-mp4.js
@@ -1,6 +1,5 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
-if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw'
 let url = futanarimp4[Math.floor(Math.random() * futanarimp4.length)]
 conn.sendFile(m.chat, 
 url, null, `*_ACA TIENES UNA RICA FUTANARI 🔥_*`, m)
