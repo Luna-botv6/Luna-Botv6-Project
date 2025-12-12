c56eebe64dee8fa0ef6edbd0b3ce49763b4fa09d
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2025-12-11 21:14:22 -0300

    Update herramientas-hd.js

--- plugins/herramientas-hd.js
+++ plugins/herramientas-hd.js
@@ -1,53 +1,41 @@
-import FormData from "form-data";
-import Jimp from "jimp";
+import fs from "fs"
+import axios from "axios"
+import uploadImage from "../src/libraries/uploadImage.js"
 
+const handler = async (m, { conn, usedPrefix, command }) => {
 
-const handler = async (m, {conn, usedPrefix, command}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.herramientas_hd
+  try {
+    const q = m.quoted ? m.quoted : m
+    const mime = (q.msg || q).mimetype || q.mediaType || ""
 
- try {    
-  let q = m.quoted ? m.quoted : m;
-  let mime = (q.msg || q).mimetype || q.mediaType || "";
-  if (!mime) throw `${tradutor.texto1} ${usedPrefix + command}*`;
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `${tradutor.texto2[0]} (${mime}) ${tradutor.texto2[1]}`;
-  m.reply(tradutor.texto3);
-  let img = await q.download?.();
-  let pr = await remini(img, "enhance");
-  conn.sendMessage(m.chat, {image: pr}, {quoted: m});
- } catch {
-  throw tradutor.texto4;
- }
-};
-handler.help = ["remini", "hd", "enhance"];
-handler.tags = ["ai", "tools"];
-handler.command = ["remini", "hd", "enhance"];
-export default handler;
+    if (!mime) throw `${usedPrefix + command}*`
+    if (!/image\/(jpe?g|png)/.test(mime)) throw `(${mime})`
 
-async function remini(imageData, operation) {
-  return new Promise(async (resolve, reject) => {
-    const availableOperations = ["enhance", "recolor", "dehaze"];
-    if (availableOperations.includes(operation)) {
-      operation = operation;
-    } else {
-      operation = availableOperations[0];
+    
+    const img = await q.download()
+    const fileUrl = await uploadImage(img)
+    const banner = await upscaleWithStellar(fileUrl)
+
+    await conn.sendMessage(m.chat, { image: banner }, { quoted: m })
+  } catch (e) {
+    throw tradutor.texto4 + e
+  }
+}
+
+handler.help = ["remini", "hd", "enhance"]
+handler.tags = ["ai", "tools"]
+handler.command = ["remini", "hd", "enhance"]
+export default handler
+
+async function upscaleWithStellar(url) {
+  const endpoint = `https://api.stellarwa.xyz/tools/upscale?url=${url}&key=BrunoSobrino`
+
+  const { data } = await axios.get(endpoint, {
+    responseType: "arraybuffer",
+    headers: {
+      accept: "image/*"
     }
-    const baseUrl = "https://inferenceengine.vyro.ai/" + operation + ".vyro";
-    const formData = new FormData();
-    formData.append("image", Buffer.from(imageData), {filename: "enhance_image_body.jpg", contentType: "image/jpeg"});
-    formData.append("model_version", 1, {"Content-Transfer-Encoding": "binary", contentType: "multipart/form-data; charset=utf-8"});
-    formData.submit({url: baseUrl, host: "inferenceengine.vyro.ai", path: "/" + operation, protocol: "https:", headers: {"User-Agent": "okhttp/4.9.3", Connection: "Keep-Alive", "Accept-Encoding": "gzip"}},
-      function (err, res) {
-        if (err) reject(err);
-        const chunks = [];
-        res.on("data", function (chunk) {chunks.push(chunk)});
-        res.on("end", function () {resolve(Buffer.concat(chunks))});
-        res.on("error", function (err) {
-        reject(err);
-        });
-      },
-    );
-  });
+  })
+
+  return Buffer.from(data)
 }
