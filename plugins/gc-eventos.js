7e8f3c8118dffbda7186c88990b955aac4707453
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2025-12-11 23:42:40 -0300

    Update gc-eventos.js

--- plugins/gc-eventos.js
+++ plugins/gc-eventos.js
@@ -6,4 +6,14 @@ handler.command = /^(evento|eventos|eventofutabuclub)$/i
 handler.group = true;
 export default handler
 
-global.eventos = `Nada aún!.`
+global.eventos = `*_Limpieza de Miembros Inactivos (7 de Febrero 2026)_*
+
+Esto se hace como un aviso para empezar a hacer activo en los meses que faltan antes que llegue el dia, traten de ser activos constantemente y aportar una que otra vez contenido. Si son personas ocupadas, traten de pasarse aquí para hablar de vez en cuando.
+
+Si ocurre algún inconveniente durante los siguientes días/meses/semanas debido a cosas de trabajo, escolares, familiares, etc.:
+
+Guarden este link en un portapapeles para recordarlo y pegarlo en WhatsApp; al tocarlo abrirá un chat privado conmigo:
+
+*GUARDA ESTE LINK*: wa.me/+5492604849203
+
+Buena suerte si sobrevives a la purga ese día ;).`
