2935ed46fed1063cdf3f739d4699604c79b8963c
Luna-botv6 <lunabotv6@gmail.com>
2026-02-28 19:04:50 -0300

    Refactor group name setting handler

--- plugins/gc-setname.js
+++ plugins/gc-setname.js
@@ -1,26 +1,28 @@
-import Presence from "@whiskeysockets/baileys";
+import fs from 'fs'
+import { getGroupDataForPlugin } from '../lib/funcion/pluginHelper.js'
 
+const handler = async (m, { conn, args }) => {
 
-const handler = async (m, {conn, args, text}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.gc_setname
+const datas = global
+const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
+const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
+const tradutor = _translate.plugins.gc_setname
 
-  if (!text) throw tradutor.texto1;
-  try {
-    const text = args.join` `;
-    if (!args || !args[0]) {
-    } else {
-      conn.groupUpdateSubject(m.chat, text);
-    }
-  } catch (e) {
-    throw tradutor.texto2;
-  }
-};
-handler.help = ['setname <text>'];
-handler.tags = ['group'];
-handler.command = /^(setname)$/i;
-handler.group = true;
-handler.admin = true;
-export default handler;
+const { isAdmin, isBotAdmin } = await getGroupDataForPlugin(conn, m.chat, m.sender)
+
+if (!isAdmin) throw tradutor.admin
+if (!isBotAdmin) throw tradutor.botadmin
+
+if (!args[0]) throw tradutor.texto1
+
+const text = args.join(' ')
+await conn.groupUpdateSubject(m.chat, text)
+
+}
+
+handler.help = ['setname <text>']
+handler.tags = ['group']
+handler.command = /^(setname)$/i
+handler.group = true
+
+export default handler
