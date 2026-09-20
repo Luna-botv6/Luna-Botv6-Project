81cdfcdabf0cbf1ec338f6d43b8f1d361daf2166
Luna-botv6 <lunabotv6@gmail.com>
2026-09-19 22:10:22 -0300

    fix-perfil

--- plugins/owner-setppbot.js
+++ plugins/owner-setppbot.js
@@ -1,30 +1,12 @@
-import { Jimp } from 'jimp';
-
 const handler = async (m, { conn, usedPrefix, command }) => {
   const _tr = await global.loadTranslation(global.getIdioma?.(m) || 'es');
   const t = _tr?.plugins?.owner_setppbot || {};
   try {
-    const botJid = conn.user.jid;
     if (!m.quoted) throw (t.sin_imagen?.replace('{comando}', usedPrefix + command) || `*[❗INFO❗] NO SE ENCONTRO LA IMAGEN, POR FAVOR RESPONDE A UNA IMAGEN USANDO EL COMANDO ${usedPrefix + command}*`);
 
-    const quoted = m.quoted;
-    const media = await quoted.download();
+    const media = await m.quoted.download();
 
-    async function resizeImage(buffer) {
-      const image = await Jimp.read(buffer);
-      const resized = image.width > image.height
-        ? image.resize({ w: 720 })
-        : image.resize({ h: 720 });
-      return { img: await resized.getBuffer('image/jpeg') };
-    }
-
-    const { img } = await resizeImage(media);
-
-    await conn.query({
-      tag: 'iq',
-      attrs: { to: botJid, type: 'set', xmlns: 'w:profile:picture' },
-      content: [{ tag: 'picture', attrs: { type: 'image' }, content: img }]
-    });
+    await conn.updateProfilePicture(conn.user.jid, media);
 
     m.reply((t.exito || '*[❗INFO❗] SE CAMBIO CON EXITO LA FOTO DE PERFIL DEL NUMERO DEL BOT*'));
   } catch {
